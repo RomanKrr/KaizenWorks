bad4a0c1195b59d4867f145f133afe2951e0ea67
RomanKrr <158074611+RomanKrr@users.noreply.github.com>
2025-02-06 16:23:28 +0200

    first build

---
+++ backend/src/index.js
@@ -0,0 +1,41 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+import path from "path"
+
+import { connectDB } from "./lib/db.js";
+import adminRoute from "./routes/admin.route.js"
+import productRoute from "./routes/products.route.js"
+
+const app = express();
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+
+dotenv.config()
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+
+app.use("/api/admin", adminRoute);
+app.use("/api/products", productRoute);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    });
+}
+
+
+app.listen(PORT, () =>{
+    console.log("Hello back", PORT);
+    connectDB();
+})

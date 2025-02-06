9fc867437da758484acd79fc4ddf9e65ef68d150
RomanKrr <158074611+RomanKrr@users.noreply.github.com>
2025-02-06 16:27:58 +0200

    without tr

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/Header/header'
+// import Header from '../components/Header/header'
 import Banner from '../components/Banner/Banner'
 import ImagesExample from '../components/ImagesExample/ImagesExample'
 import About from '../components/About/About'

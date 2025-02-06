80551038a3bfd45d57fe049e2b4443cafa0a7482
RomanKrr <158074611+RomanKrr@users.noreply.github.com>
2025-02-06 20:06:06 +0200

    f

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import { connectDB } from "./lib/db.js";
 import adminRoute from "./routes/admin.route.js"
 import productRoute from "./routes/products.route.js"
 
+dotenv.config();
 const app = express();
 app.use(cors({
     origin: "http://localhost:5173",
@@ -17,7 +18,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 
-dotenv.config()
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
